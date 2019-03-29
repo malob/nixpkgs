373746bb6fafd18a49fe5dd5a9ca9536adfe40aa
Malo Bourgon <mbourgon@gmail.com>
2019-03-29 05:18:06 +0000

    Add luacheck

--- overlays/usersetup.nix
+++ overlays/usersetup.nix
@@ -2,15 +2,19 @@ self: super:
 {
   hie = super.pkgs.callPackage ../pkgs/hie-nix {};
   myNodePackages =  super.pkgs.callPackage ../pkgs/node-packages {};
+
   usersetup = self.buildEnv {
     name = "UserSetup";
     paths = with self.pkgs; [
       bat
+      hie.hies
       kitty
       neovim
       neovim-remote
       tldr
-      hie.hies
+
+      luaPackages.luacheck
+
       myNodePackages.bash-language-server
       myNodePackages.typescript
       myNodePackages.typescript-language-server
