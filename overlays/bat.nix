4ecb489253b7a4f74542df1ad1a5da2bf37b6361
Malo Bourgon <mbourgon@gmail.com>
2019-04-29 18:14:13 -0700

    Update bat config

--- overlays/bat.nix
+++ overlays/bat.nix
@@ -1,11 +1,18 @@
 self: super:
-{
+let
+  batConfig = ''
+    --theme="Monokai Extended"
+    --style="plain"
+  '';
+in {
   myBat = super.pkgs.symlinkJoin {
     name = "myBat";
     paths = [ self.pkgs.bat ];
     buildInputs = [ super.pkgs.makeWrapper ];
     postBuild = ''
-      wrapProgram $out/bin/bat --add-flags "--style plain";
+      mkdir -p "$out/.config/bat"
+      echo '${batConfig}' > "$out/.config/bat/bat.conf"
+      wrapProgram $out/bin/bat --set BAT_CONFIG_PATH "$out/.config/bat/bat.conf"
     '';
   };
 }
