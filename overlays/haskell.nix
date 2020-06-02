b19ca3adc637805f03d534b849879c304428d2bb
Malo Bourgon <mbourgon@gmail.com>
2020-06-01 20:43:59 -0700

    Update sources, and small tweaks

--- overlays/haskell.nix
+++ overlays/haskell.nix
@@ -2,7 +2,7 @@ self: super: {
   myHaskellEnv = super.buildEnv {
     name  = "HaskellEnv";
     paths = with self.pkgs; [
-      (hies.unstableFallback.selection { selector = p: { inherit (p) ghc882 ghc865; }; })
+      (hies.selection { selector = p: { inherit (p) ghc882 ghc865; }; })
       unstable.haskellPackages.hoogle
       unstable.haskellPackages.hpack
       unstable.stack
