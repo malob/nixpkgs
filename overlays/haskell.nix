27b6d933eb08487ce8fa76e1cd76b2804535bc4e
Malo Bourgon <mbourgon@gmail.com>
2020-08-10 11:18:56 -0700

    Remove hls and hie sources, and update to use hls from nixpkgs

--- overlays/haskell.nix
+++ overlays/haskell.nix
@@ -1,12 +1,12 @@
 self: super: {
   myHaskellEnv = super.buildEnv {
     name  = "HaskellEnv";
-    paths = with self.pkgs; [
-      (hies.selection { selector = p: { inherit (p) ghc882 ghc865; }; })
-      ((hls { version = "8.8.3"; }).exes.haskell-language-server)
-      unstable.haskellPackages.hoogle
-      unstable.haskellPackages.hpack
-      unstable.stack
+    paths = with self.pkgs.unstable; [
+      haskellPackages.haskell-language-server
+      haskellPackages.hoogle
+      haskellPackages.hpack
+      haskellPackages.implicit-hie
+      stack
     ];
   };
 }
