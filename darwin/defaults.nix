92010840d8b699e16a048739640f3c5ee122b06d
Malo Bourgon <mbourgon@gmail.com>
2020-09-22 14:27:17 -0700

    Move nix-darwin defaults experiments to a branch

--- darwin/defaults.nix
+++ darwin/defaults.nix
@@ -1,28 +1,10 @@
-{...}:
+{ ... }:
 
 {
-  # Module disabled because I'm using patched versions not yet upstreamed
-  disabledModules = [
-    "system/defaults/NSGlobalDomain.nix"
-    "system/defaults/alf.nix"
-    "system/defaults/finder.nix"
-    "system/defaults/trackpad.nix"
-    "system/defaults-write.nix"
-  ];
-
-  # Patched versions of disabled modules
-  imports = [
-    ./modules/system/defaults/NSGlobalDomain.nix
-    ./modules/system/defaults/alf.nix
-    ./modules/system/defaults/finder.nix
-    ./modules/system/defaults/trackpad.nix
-    ./modules/system/defaults-write.nix
-  ];
-
   system.defaults.NSGlobalDomain = {
     "com.apple.trackpad.scaling"         = "3.0";
     AppleMeasurementUnits                = "Centimeters";
-    AppleMetricUnits                     = true;
+    AppleMetricUnits                     = 1;
     AppleShowScrollBars                  = "Automatic";
     AppleTemperatureUnit                 = "Celsius";
     InitialKeyRepeat                     = 15;
@@ -35,9 +17,9 @@
   # Firewall
   system.defaults.alf = {
     globalstate                = 1;
-    allowsignedenabled         = true;
-    allowdownloadsignedenabled = true;
-    stealthenabled             = true;
+    allowsignedenabled         = 1;
+    allowdownloadsignedenabled = 1;
+    stealthenabled             = 1;
   };
 
   # Dock and Mission Control
@@ -61,20 +43,10 @@
   system.defaults.trackpad = {
     Clicking                = false;
     TrackpadRightClick      = true;
-    AppleMultitouchTrackpad = 0;
   };
 
   # Finder
   system.defaults.finder = {
-    ShowHardDrivesOnDesktop              = false;
-    ShowExternalHardDrivesOnDesktop      = false;
-    ShowRemovableMediaOnDesktop          = false;
-    ShowMountedServersOnDesktop          = false;
     FXEnableExtensionChangeWarning       = true;
-    FXEnableRemoveFromICloudDriveWarning = true;
-    FXRemoveOldTrashItems                = true;
-    WarnOnEmptyTrash                     = true;
-    _FXSortFoldersFirst                  = true;
-    _FXSortFoldersFirstOnDesktop         = true;
   };
 }
