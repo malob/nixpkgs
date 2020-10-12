d4f59f6655862006e1565a9c5570d4d65de780ef
Malo Bourgon <mbourgon@gmail.com>
2020-10-12 12:37:51 -0700

    Add more binary caches

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -37,10 +37,14 @@
   nix.binaryCaches = [
     "https://cache.nixos.org/"
     "https://iohk.cachix.org"
+    "https://hydra.iohk.io"
+    "https://malo.cachix.org"
   ];
   nix.binaryCachePublicKeys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
+    "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    "malo.cachix.org-1:fJL4+lpyMs/1cdZ23nPQXArGj8AS7x9U67O8rMkkMIo="
   ];
   nix.trustedUsers = [
     "@admin"
