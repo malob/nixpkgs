3b904f782eef38243cb41d24a9330955c550e614
Malo Bourgon <mbourgon@gmail.com>
2021-04-12 15:04:42 -0700

    Add shell-functools

--- home/default.nix
+++ home/default.nix
@@ -63,6 +63,7 @@
     mosh # wrapper for `ssh` that better and not dropping connections
     nodePackages.speed-test # nice speed-test tool
     parallel # runs commands in parallel
+    python3Packages.shell-functools # a collection of functional programming tools for the shell
     stable.procs # fancy version of `ps`
     ripgrep # better version of `grep`
     tealdeer # rust implementation of `tldr`
