3f04d3e7f24393147269e98f6ca5fb7af9ed61c0
Malo Bourgon <mbourgon@gmail.com>
2021-04-22 12:26:03 -0700

    Reorganize Homebrew configuration

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -14,6 +14,42 @@
     "homebrew/services"
   ];
 
+  # Prefer installing application from the Mac App Store
+  #
+  # Commented apps suffer continual update issue:
+  # https://github.com/malob/nixpkgs/issues/9
+  homebrew.masApps = {
+    # "1Blocker" = 1365531024;
+    "1Password" = 1333542190;
+    "Accelerate for Safari" = 1459809092;
+    "Apple Configurator 2" = 1037126344;
+    # DaisyDisk = 411643860;
+    "Dark Mode for Safari" = 1397180934;
+    Deliveries = 290986013;
+    Fantastical = 975937182;
+    "Gemini 2" = 1090488118;
+    "iMazing Profile Editor" = 1487860882;
+    Keynote = 409183694;
+    "LG Screen Manager" = 1142051783;
+    MindNode = 1289197285;
+    Numbers = 409203825;
+    Pages = 409201541;
+    Patterns = 429449079;
+    "Pixelmator Classic" = 407963104;
+    "Pixelmator Pro" = 1289583905;
+    "Save to Raindrop.io" = 1549370672;
+    Slack = 803453959;
+    SiteSucker = 442168834;
+    "Things 3" = 904280696;
+    Ulysses = 1225570693;
+    Vimari = 1480933944;
+    "WiFi Explorer" = 494803304;
+    Xcode = 497799835;
+    "Yubico Authenticator" = 1497506650;
+  };
+
+  # If an app isn't available in the Mac App Store, or the version in the App Store has
+  # limitiations, e.g., Transmit, install the Homebrew Cask.
   homebrew.casks = [
     "atom"
     "amethyst"
@@ -50,35 +86,7 @@
     "yubico-yubikey-personalization-gui"
   ];
 
-  # Commented apps suffer continual update issue:
-  # https://github.com/malob/nixpkgs/issues/9
-  homebrew.masApps = {
-    # "1Blocker" = 1365531024;
-    "1Password" = 1333542190;
-    "Accelerate for Safari" = 1459809092;
-    "Apple Configurator 2" = 1037126344;
-    # DaisyDisk = 411643860;
-    "Dark Mode for Safari" = 1397180934;
-    Deliveries = 290986013;
-    Fantastical = 975937182;
-    "Gemini 2" = 1090488118;
-    "iMazing Profile Editor" = 1487860882;
-    Keynote = 409183694;
-    "LG Screen Manager" = 1142051783;
-    MindNode = 1289197285;
-    Numbers = 409203825;
-    Pages = 409201541;
-    Patterns = 429449079;
-    "Pixelmator Classic" = 407963104;
-    "Pixelmator Pro" = 1289583905;
-    "Save to Raindrop.io" = 1549370672;
-    Slack = 803453959;
-    SiteSucker = 442168834;
-    "Things 3" = 904280696;
-    Ulysses = 1225570693;
-    Vimari = 1480933944;
-    "WiFi Explorer" = 494803304;
-    Xcode = 497799835;
-    "Yubico Authenticator" = 1497506650;
-  };
+  # For cli packages that aren't currently available for macOS in `nixpkgs`.Packages should be
+  # installed in `../home/default.nix` whenever possible.
+  homebrew.brews = [];
 }
