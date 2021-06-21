d6126d97ab348176c94589ec421be05d97e2822c
Malo Bourgon <mbourgon@gmail.com>
2021-06-21 10:18:34 -0700

    Update deprecated home-manager option

--- home/default.nix
+++ home/default.nix
@@ -32,7 +32,7 @@
   # https://direnv.net
   # https://rycee.gitlab.io/home-manager/options.html#opt-programs.direnv.enable
   programs.direnv.enable = true;
-  programs.direnv.enableNixDirenvIntegration = true;
+  programs.direnv.nix-direnv.enable = true;
 
   # Htop
   # https://rycee.gitlab.io/home-manager/options.html#opt-programs.htop.enable
