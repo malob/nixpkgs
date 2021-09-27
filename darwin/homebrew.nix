0201b2693ebf9b548984eb99af389d8e095c5cf6
Malo Bourgon <mbourgon@gmail.com>
2021-09-27 11:32:17 -0700

    Add Discord, disable `mas` until macOS 12 fix

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -24,36 +24,39 @@ in
   #
   # Commented apps suffer continual update issue:
   # https://github.com/malob/nixpkgs/issues/9
-  homebrew.masApps = {
-    # "1Blocker" = 1365531024;
-    "1Password" = 1333542190;
-    "Accelerate for Safari" = 1459809092;
-    "Apple Configurator 2" = 1037126344;
-    DaisyDisk = 411643860;
-    "Dark Mode for Safari" = 1397180934;
-    Deliveries = 290986013;
-    Fantastical = 975937182;
-    "Gemini 2" = 1090488118;
-    "iMazing Profile Editor" = 1487860882;
-    Keynote = 409183694;
-    "LG Screen Manager" = 1142051783;
-    MindNode = 1289197285;
-    Numbers = 409203825;
-    Pages = 409201541;
-    Patterns = 429449079;
-    "Pixelmator Classic" = 407963104;
-    "Pixelmator Pro" = 1289583905;
-    "Save to Raindrop.io" = 1549370672;
-    Slack = 803453959;
-    SiteSucker = 442168834;
-    "Things 3" = 904280696;
-    TripMode = 1513400665;
-    Ulysses = 1225570693;
-    Vimari = 1480933944;
-    "WiFi Explorer" = 494803304;
-    Xcode = 497799835;
-    "Yubico Authenticator" = 1497506650;
-  };
+  #
+  # `mas` not working on macOS 12 beta 7
+  # https://github.com/mas-cli/mas/issues/417
+  # homebrew.masApps = {
+  #   # "1Blocker" = 1365531024;
+  #   "1Password" = 1333542190;
+  #   "Accelerate for Safari" = 1459809092;
+  #   "Apple Configurator 2" = 1037126344;
+  #   DaisyDisk = 411643860;
+  #   "Dark Mode for Safari" = 1397180934;
+  #   Deliveries = 290986013;
+  #   Fantastical = 975937182;
+  #   "Gemini 2" = 1090488118;
+  #   "iMazing Profile Editor" = 1487860882;
+  #   Keynote = 409183694;
+  #   "LG Screen Manager" = 1142051783;
+  #   MindNode = 1289197285;
+  #   Numbers = 409203825;
+  #   Pages = 409201541;
+  #   Patterns = 429449079;
+  #   "Pixelmator Classic" = 407963104;
+  #   "Pixelmator Pro" = 1289583905;
+  #   "Save to Raindrop.io" = 1549370672;
+  #   Slack = 803453959;
+  #   SiteSucker = 442168834;
+  #   "Things 3" = 904280696;
+  #   TripMode = 1513400665;
+  #   Ulysses = 1225570693;
+  #   Vimari = 1480933944;
+  #   "WiFi Explorer" = 494803304;
+  #   Xcode = 497799835;
+  #   "Yubico Authenticator" = 1497506650;
+  # };
 
   # If an app isn't available in the Mac App Store, or the version in the App Store has
   # limitiations, e.g., Transmit, install the Homebrew Cask.
@@ -65,6 +68,7 @@ in
     "balenaetcher"
     "camo-studio"
     "cloudflare-warp"
+    "discord"
     "displaycal"
     "firefox"
     "google-chrome"
