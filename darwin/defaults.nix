fae0af40cf42a9fef0979aa6891b225451d3d840
Malo Bourgon <mbourgon@gmail.com>
2022-01-18 10:32:10 -0800

    Add macOS defaults config for hot corners

--- darwin/defaults.nix
+++ darwin/defaults.nix
@@ -30,6 +30,11 @@
     expose-group-by-app = false;
     mru-spaces = false;
     tilesize = 128;
+    # Disable all hot corners
+    wvous-bl-corner = 1;
+    wvous-br-corner = 1;
+    wvous-tl-corner = 1;
+    wvous-tr-corner = 1;
   };
 
   # Login and lock screen
