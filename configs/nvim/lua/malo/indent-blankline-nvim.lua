9e158073549bb15ec6e600b9ffcadeab14db9ab3
Malo Bourgon <mbourgon@gmail.com>
2022-01-31 18:32:47 -0800

    Disable indent-blankline.nvim in terminal buffers

--- configs/nvim/lua/malo/indent-blankline-nvim.lua
+++ configs/nvim/lua/malo/indent-blankline-nvim.lua
@@ -5,6 +5,7 @@ vim.cmd 'packadd indent-blankline.nvim'
 vim.g.indent_blankline_char = '│'
 vim.g.indent_blankline_show_first_indent_level = true
 vim.g.indent_blankline_show_trailing_blankline_indent = false
+vim.g.indent_blankline_buftype_exclude = { 'terminal' }
 vim.g.indent_blankline_filetype_exclude = { 'help', 'markdown' }
 
 vim.g.indent_blankline_use_treesitter = true
