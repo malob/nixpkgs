334bd37635591760f84a6da9ccb4b4e5c5cbf4b0
Malo Bourgon <mbourgon@gmail.com>
2022-03-14 13:25:42 -0700

    Add nix options for better direnv experience

--- darwin/bootstrap.nix
+++ darwin/bootstrap.nix
@@ -21,6 +21,8 @@
   nix.extraOptions = ''
     auto-optimise-store = true
     experimental-features = nix-command flakes
+    keep-outputs = true
+    keep-derivations = true
   '' + lib.optionalString (pkgs.system == "aarch64-darwin") ''
     extra-platforms = x86_64-darwin aarch64-darwin
   '';
