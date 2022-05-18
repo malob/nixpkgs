eced8eecc199adf602545a870e85c65bda744609
Malo Bourgon <mbourgon@gmail.com>
2022-05-18 15:01:04 -0700

    Move to release version of 1Password 8

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -79,7 +79,7 @@ in
   # If an app isn't available in the Mac App Store, or the version in the App Store has
   # limitiations, e.g., Transmit, install the Homebrew Cask.
   homebrew.casks = [
-    "1password-beta"
+    "1password"
     "1password-cli"
     "amethyst"
     "anki"
