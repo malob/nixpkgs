2c1a8183cc411953ed75b54614e19ec8612545d7
Malo Bourgon <mbourgon@gmail.com>
2022-05-21 12:02:14 -0700

    Fix starship battery error

--- home/starship.nix
+++ home/starship.nix
@@ -7,7 +7,6 @@
     # See docs here: https://starship.rs/config/
     # Symbols config configured ./starship-symbols.nix.
 
-    battery.display.threshold = 25; # display battery information if charge is <= 25%
     directory.fish_style_pwd_dir_length = 1; # turn on fish directory truncation
     directory.truncation_length = 2; # number of directories not to truncate
     gcloud.disabled = true; # annoying to always have on
