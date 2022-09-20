8941d4adac5bc75b8e450e50fe99fcef885bdf15
Malo Bourgon <mbourgon@gmail.com>
2022-09-20 17:35:19 -0400

    Add `shlvl` to Starship prompt

--- home/starship.nix
+++ home/starship.nix
@@ -12,6 +12,7 @@
     gcloud.disabled = true; # annoying to always have on
     hostname.style = "bold green"; # don't like the default
     memory_usage.disabled = true; # because it includes cached memory it's reported as full a lot
+    shlvl.disabled = false;
     username.style_user = "bold blue"; # don't like the default
   };
 }
