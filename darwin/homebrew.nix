2a9af24edd11a9f270375882ba917804732000eb
Malo Bourgon <mbourgon@gmail.com>
2022-10-20 10:18:17 -0700

    Cleanup apps installed by Homebrew

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -39,38 +39,25 @@ in
   ];
 
   # Prefer installing application from the Mac App Store
-  #
-  # Commented apps suffer continual update issue:
-  # https://github.com/malob/nixpkgs/issues/9
   homebrew.masApps = {
-    # "1Blocker" = 1365531024;
-    # "1Password" = 1333542190;
     "1Password for Safari" = 1569813296;
     "Accelerate for Safari" = 1459809092;
-    # "Apple Configurator 2" = 1037126344;
     DaisyDisk = 411643860;
     "Dark Mode for Safari" = 1397180934;
     Deliveries = 290986013;
     Fantastical = 975937182;
-    # "Gemini 2" = 1090488118;
-    # "iMazing Profile Editor" = 1487860882;
     Keynote = 409183694;
-    # MindNode = 1289197285;
     Numbers = 409203825;
     Pages = 409201541;
     Patterns = 429449079;
-    # "Pixelmator Classic" = 407963104;
     "Pixelmator Pro" = 1289583905;
     "Save to Raindrop.io" = 1549370672;
     Slack = 803453959;
-    # SiteSucker = 442168834;
+    "Swift Playgrounds" = 1496833156;
     "Things 3" = 904280696;
-    # TripMode = 1513400665;
-    # Ulysses = 1225570693;
     Vimari = 1480933944;
     "WiFi Explorer" = 494803304;
-    # TODO: Re-enable once macOS Ventura officially launches
-    # Xcode = 497799835;
+    Xcode = 497799835;
     "Yubico Authenticator" = 1497506650;
   };
 
@@ -79,7 +66,6 @@ in
   homebrew.casks = [
     "1password"
     "1password-cli"
-    "amethyst"
     "anki"
     "arq"
     "balenaetcher"
@@ -93,16 +79,15 @@ in
     "gpg-suite"
     "hammerspoon"
     "keybase"
+    # "loopback" -- TODO: re-enable when macOS 13 version ships
     "nvidia-geforce-now"
     "obsbot-me-tool"
     "obsbot-tinycam"
-    "obsidian"
     "parallels"
     "postman"
     "protonvpn"
     "raindropio"
     "raycast"
-    "secretive"
     "signal"
     "skype"
     "steam"
