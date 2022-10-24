6a1df3b4f88aec9d747f4f64fc1de521d8f19387
Malo Bourgon <mbourgon@gmail.com>
2022-10-23 17:58:25 -0700

    Tweak completions settings

--- configs/nvim/lua/malo/coq_nvim.lua
+++ configs/nvim/lua/malo/coq_nvim.lua
@@ -1,7 +1,6 @@
-local keymaps = require'malo.utils'.keymaps
-
 -- coq_nvim
 -- https://github.com/ms-jpq/coq_nvim
+
 vim.g.coq_settings = {
   auto_start = 'shut-up',
   xdg = true,
@@ -9,8 +8,19 @@ vim.g.coq_settings = {
   clients = {
     tabnine = {
       enabled = true,
-    }
-  }
+    },
+  },
+  display = {
+    preview = {
+      border = 'shadow',
+    },
+  },
+  limits = {
+    completion_auto_timeout = 0.5,
+  },
+  match = {
+    max_results = 100,
+  },
 }
 
 vim.cmd 'packadd coq_nvim'
