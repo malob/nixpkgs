f4b868579fac1840f2ef19a3daad8a30bda1196a
Dominic Rubas <rubas@users.noreply.github.com>
2023-08-13 21:53:15 +0200

    Update users.nix
    https://github.com/NixOS/nixpkgs/pull/248278

--- modules/darwin/users.nix
+++ modules/darwin/users.nix
@@ -7,19 +7,19 @@ in
 {
   options.users.primaryUser = {
     username = mkOption {
-      type = with types; nullOr string;
+      type = with types; nullOr str;
       default = null;
     };
     fullName = mkOption {
-      type = with types; nullOr string;
+      type = with types; nullOr str;
       default = null;
     };
     email = mkOption {
-      type = with types; nullOr string;
+      type = with types; nullOr str;
       default = null;
     };
     nixConfigDirectory = mkOption {
-      type = with types; nullOr string;
+      type = with types; nullOr str;
       default = null;
     };
   };
