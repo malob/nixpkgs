7c669987365ab505194c08be48fc7ecfae615eaa
Malo Bourgon <mbourgon@gmail.com>
2023-11-06 11:50:57 -0800

    Update to new `indent-blankline.nvim` config format

--- configs/nvim/lua/malo/indent-blankline-nvim.lua
+++ configs/nvim/lua/malo/indent-blankline-nvim.lua
@@ -1,14 +1,15 @@
 -- ident-blankline.nvim
 -- https://github.com/lukas-reineke/indent-blankline.nvim
 
-require'indent_blankline'.setup {
-  char = '│',
-  show_trailing_blankline_indent = false,
-  buftype_exclude = { 'terminal' },
-  filetype_exclude = { 'help', 'markdown' },
-
-  use_treesitter = true,
-  show_current_context = true,
-  context_char = '┃',
+require'ibl'.setup {
+  indent = {
+    char = '│',
+  },
+  exclude = {
+    filetypes = { 'help', 'markdown' },
+  },
+  scope = {
+  char = '┃',
+  },
 }
 
