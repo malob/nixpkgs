96bd27ff78d42e9b0ce88761b9598a7257c76b0a
Malo Bourgon <mbourgon@gmail.com>
2024-03-21 10:51:11 -0700

    Test out `difftastic` as an alternative to `delta`

--- home/git.nix
+++ home/git.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 
 {
   # Git
@@ -19,12 +19,15 @@
   programs.git.userName = config.home.user-info.fullName;
 
   # Enhanced diffs
-  programs.git.delta.enable = true;
+  # programs.git.delta.enable = true;
+  programs.git.difftastic.enable = true;
+  programs.git.difftastic.display = "inline";
 
 
   # GitHub CLI
   # https://rycee.gitlab.io/home-manager/options.html#opt-programs.gh.enable
   # Aliases config in ./gh-aliases.nix
   programs.gh.enable = true;
+  programs.gh.settings.version = 1;
   programs.gh.settings.git_protocol = "ssh";
 }
