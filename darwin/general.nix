205be5c3c8799d8e8a36227eccbc15146f11c420
Malo Bourgon <mbourgon@gmail.com>
2024-06-17 16:21:31 -0700

    Update inputs

--- darwin/general.nix
+++ darwin/general.nix
@@ -17,8 +17,7 @@
   programs.nix-index.enable = true;
 
   # Fonts
-  fonts.fontDir.enable = true;
-  fonts.fonts = with pkgs; [
+  fonts.packages = with pkgs; [
      recursive
      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
    ];
